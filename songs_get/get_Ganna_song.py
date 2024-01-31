d8e8b9b4bf9d8ea89219fba8154bcb037b7e765d
Jigar Shah <shah.jigar2002@gmail.com>
2024-01-31 13:11:01 +0530

    get songs in ganna playlist file added

---
+++ songs_get/get_Ganna_song.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_playlist_songs(playlist_url):
+    # Send a GET request to the playlist URL
+    response = requests.get(playlist_url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # soup = BeautifulSoup(html_snippet, 'html.parser')
+
+        # Find all ul elements with the class "_row list_data"
+        ul_elements = soup.find_all('ul', class_='_row list_data')
+
+        # Extract song names from each ul element
+        song_names = []
+        artist_name = []
+
+        for ul_element in ul_elements:
+            song_element = ul_element.find('a', class_='t_over')
+            if song_element:
+                song_name = song_element.text.strip()
+                song_names.append(song_name)
+            artist = ul_element.find('a', class_='_link').text
+            # a = [artist1.text.strip() for artist1 in artist]
+            # print(a)
+            artist_name.append(artist)
+
+        # return song_titles
+        return song_names,artist_name
+    else:
+        print(f"Error: Unable to fetch the playlist. Status code: {response.status_code}")
+        return None
+
+if __name__ == "__main__":
+    playlist_url = "https://gaana.com/playlist/gaana-dj-hindi-top-50-1"
+    songs, artists = get_playlist_songs(playlist_url)
+
+    if songs:
+        print("Songs in the playlist:")
+        # for index, song in enumerate(songs, start=1):
+        #     print(f"{song}")
+        lst = []
+        for i in songs:
+            t = i.split("Premium  ")
+            lst.append(t[1])
+            # print(t[1])
+        print(lst)
+        print(artists)
