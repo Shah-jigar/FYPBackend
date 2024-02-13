ea03b5ab6655ab143b0786fb134bcc49178518fc
Shah-jigar <shah.jigar2002@gmail.com>
2024-02-13 21:00:43 +0530

    updated gitignore file

--- songs_get/get_Ganna_song.py
+++ songs_get/get_Ganna_song.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+from pprint import pprint
 def get_playlist_songs(playlist_url):
     # Send a GET request to the playlist URL
     response = requests.get(playlist_url)
@@ -12,40 +12,45 @@ def get_playlist_songs(playlist_url):
         # soup = BeautifulSoup(html_snippet, 'html.parser')
 
         # Find all ul elements with the class "_row list_data"
-        ul_elements = soup.find_all('ul', class_='_row list_data')
-
+        ul_elements = soup.find_all('div', class_='_grp')[1:]
+        # pprint(ul_elements)
         # Extract song names from each ul element
         song_names = []
-        artist_name = []
-
+        artist_names = []
         for ul_element in ul_elements:
-            song_element = ul_element.find('a', class_='t_over')
-            if song_element:
-                song_name = song_element.text.strip()
-                song_names.append(song_name)
-            artist = ul_element.find('a', class_='_link').text
-            # a = [artist1.text.strip() for artist1 in artist]
-            # print(a)
-            artist_name.append(artist)
+            artist_name = ""
+            # song_name = ul_element.find('span', class_='t_over').get_text(strip=True)
+            span_element = ul_element.find('span', class_='t_over')
+            # Remove the inner <span> element if it exists
+            inner_span = span_element.find('span', class_='new_premium')
+            if inner_span:
+                inner_span.extract()
+            # Extract the text
+            song_name = span_element.get_text(strip=True)
+            song_names.append(song_name)
+            # Extract the artist name
+            artist_name_elements = ul_element.find_all('a', class_='_link')
+            artist_names.append(', '.join([artist.get_text(strip=True) for artist in artist_name_elements]))
 
         # return song_titles
-        return song_names,artist_name
+        return song_names,artist_names
     else:
         print(f"Error: Unable to fetch the playlist. Status code: {response.status_code}")
         return None
 
 if __name__ == "__main__":
-    playlist_url = "https://gaana.com/playlist/gaana-dj-hindi-top-50-1"
+    playlist_url = "https://gaana.com/playlist/gaana-dj-2020s-happy-hits-hindi"
     songs, artists = get_playlist_songs(playlist_url)
 
     if songs:
         print("Songs in the playlist:")
         # for index, song in enumerate(songs, start=1):
         #     print(f"{song}")
-        lst = []
-        for i in songs:
-            t = i.split("Premium  ")
-            lst.append(t[1])
+        # lst = []
+        # for i in songs:
+        #     t = i.split("Premium  ")
+        #     lst.append(t[1])
             # print(t[1])
-        print(lst)
+        # print(lst)
+        print(songs)
         print(artists)
--- songs_get/youtube_download.py
+++ songs_get/youtube_download.py
@@ -1,6 +1,7 @@
 # Download songs from youtube
 
-# import yt_dlp
+import yt_dlp
+
 
 def download_music(artist, song, output_path='./downloads'):
     # Create the search query
@@ -30,21 +31,39 @@ def download_music(artist, song, output_path='./downloads'):
             print(f"Error: {e}")
 
 
-if __name__ == "__main__":
-    # songs = ['Hum Bekhudi Mein Tum Ko Pukare', 'Koi Sagar Dil Ko Bahlata Nahin', 'Rang Aur Noor Ki Barat', 'Hui Sham Unka Khayal Aa Gaya', 'Mere Mehboob Kahin Aur', 'Guzre Hain Aaj Ishq Mein Ham', 'Muddat Huyi Hai', 'Diya Yeh Dil Agar Usko', 'Yeh Na Thi Hamari Qismat', 'Nukta Chin Hai', 'Baithe Hain', 'Falsafe Ishq Mein Pesh Aaye Sawalon Ki Tarha', 'Dil Ki Baat Kahi Nahin Jaati', 'Jo Unki Tamanna Hai', 'Haaye Mehamaan Kahan Yeh Gham E Jana Hoga', 'Maine Jab Se Tujhe Aye Jaan E Ghazal', 'Zikar Us Pariwash Ka', 'Dard Minnat Kashe', 'Humko Mita Sake Yeh Zamane Mein Dam Nahin', 'Shauq Har Rang', 'Ek Hi Baat Zamane Ki Kitabon Mein Nahin', 'Ghazab Kiya Tere Waade Pe', 'Bas Ke Dushwar Hai', 'Zindagi Aaj Mere Naam Se', 'Jab Tere Pyarke Afsana', 'Kisi Ki Yaad Mein Payi Hain', 'Tod Do Ahd E Mohabbat', 'Jalwo Waqt', 'Aaj Mere Kareeb', 'Main Deep Jalaye Baitha Hoon', 'Poochh Na Mujh Se', 'Chhalke Teri Ankhon Se', 'Na Kisi Ki Aankh Ka Noor Hoon', 'Jane Kya Dhoondti Rahti Hai', 'Lagta Nahin Dil Mera']
-    # artist_name = 'Mohammed Rafi'
+def download_video(artist, song, output_path='./downloads'):
+    # Create the search query
+    search_query = f'ytsearch:{artist} {song}'
 
-    songs1 = ['Udd Jaa Kaale Kaava (From "Gadar 2")', 'Leke Prabhu Ka Naam (From "Tiger 3")',  'Phir Aur Kya Chahiye (From "Zara Hatke Zara Bachke")', 'Tum Kya Mile (From "Rocky Aur Rani Kii Prem Kahaani")']
-    artists = ['Sukhbir', 'Aur', 'Neeti Mohan']
-    print("Songs count: " + str(len(songs1)))
-    print("Artist count: " + str(len(artists)))
-    # song_name = 'Main Bhi Shaamil Tha Gunahegaaron Mein'
-    # for song in songs:
-    #     download_music(artist_name, song)
-    # for i in range(0,len(songs)):
-    #     download_music(artists[i], songs1[i])
+    # Set up options for yt-dlp to download in mp4 format
+    ydl_opts = {
+        'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',
+        'outtmpl': f'{output_path}/{artist} - {song}.%(ext)s',
+        'noplaylist': True,
+        'quiet': False,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            ydl.download([search_query])
+            print("Download complete.")
+        except yt_dlp.DownloadError as e:
+            print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    output_path = './downloads/emotion/sad'
+
+    Songs =  ['Ranjha Bichda', 'Tanhaiyyan - A LOVE BALLAD', 'Main Nahi Hoon', 'Barbaad', 'Lambi Judaai 2.0', 'Jhoota Tera Pyar', 'Tujhe Yaad Na Meri Ayee - 2', 'Kalleyan', 'Faasla', 'Sun Ranjheya', 'Nindiya', 'Kya Loge Tum', 'Ek Hi Toh Dil', 'Lost', 'Zohrajabeen', 'Haaye Dard', 'Yaad Aati Hai', 'Filhall', 'Baarish Ki Jaaye', 'Tauba Meri Tauba', 'Tubhi Royega', 'Jaana', 'Aate Rehte Hain', 'Tu Kaun Hai', 'Toot Jaaun', 'Tabah Kar Gaye', 'Tere Pyar Mein (feat. Vineet Raina & Alisha Panwar)', 'Rootha Re', 'Filhaal2 Mohabbat', 'Bidaai', 'Pyaar Karte Ho Na', 'Bairagi Mann', 'Maar Sutteya', 'Duur', 'Dil Todta Hai', 'Manzoor', 'Duniya', 'Theherjaa', 'Mila Na Dil Ko', 'Das Ki Kasoor', 'Saadgi To Hamari', 'Alvida', 'Yaad', 'Chalo Theek Hai', 'Dil Sambhalta Nahin', 'Kuch Tumhara Kuch Hamara', 'Baarishon Mein', 'Muskuraa Lena Tum', 'Ro Rahi Hu', 'Woh Tere Pyar Ka Gham', 'Kahin Na Lage', 'Iss Baarish Mein Unplugged By Yasser Desai', 'Mubarak Ho', 'Dhokhha', 'Hayaa', 'Darbadar', 'Kashmakash', 'Raah Dikha De', 'Tenu Aunda Nahi', 'Maaf Nahi Karega', 'Ishq Samundar', 'Kajal', 'Alvida', 'Minnatein', 'Ishq Ki Baarish', 'Tujhe Chaahta Hoon Kyun', 'Jaana Hai Toh Jaa', 'Kirdaar, Episode 2', 'Tutt Gaya', 'Namonishan', 'Bewafa', 'Dil Toota Hi Raha', 'Arsaa', 'HeartWoken', 'Maan Le (From "Chitrakut")', 'Jo Hoga Accha Hoga, Episode 1', 'Saasein Thami Hai (From "Damned Graveyard")', 'Bechari', 'Kayi Din Huye', 'Kya Kar Diya', 'Dhokebaaz', 'Jee Lenge Hum', 'The Fall', 'Jaa Rahe Ho', 'Dua Karo', 'Yeh Kyun Kiya', 'Saamna', 'Main Royaan', 'Yaad Aati Nahin', 'Tere Bin Jeena Kya', 'Mitra Re', 'Ishq Nahi Karte', 'Ek Tu Hi Toh Hai', 'Shayad', 'Kabhii Tumhhe', 'Rihaayi De', 'Mehrama', 'Main Tumhara', 'Pyaar Toh Tha', 'Tujhe Yaad Badi Aaungi']
+    Artists =  ['Stebin Ben, Nilesh Ahuja, Kumaar', 'Shaan', 'Ali Zafar', 'Laqshay Kapoor, Gourov Dasgupta, Kunaal Vermaa', 'Shilpa Joshi', 'Rahat Fateh Ali Khan', 'B Praak, Jaani, Jatin-Lalit', 'AKASA, Taaruk', 'Darshan Raval, Shirley Setia', 'Goldie Sohel', 'Sanchit Balhara, Ankit Balhara, Supriyaa Pathak, Atharv Bakshi, Kausar Munir', 'B Praak, Jaani, Akshay Kumar', 'Raman Romana, Hiten, Dilwala', 'Pranav Chandran', 'B Praak, Jaani', 'Darshan Raval', 'Harrdy Sandhu, Abhishek Singh', 'B Praak', 'B Praak, Nawazuddin Siddiqui, Sunanda Sharma', 'Mamta Sharma', 'Crown J', 'Jay Bhattacharya', 'B Praak, Jaani', 'Shadow And Light, Salim Merchant, Pavithra Chari', 'Anmol Daniel', 'Altamash Faridi', 'Krishna Beura, Vineet Raina, Alisha Panwar', 'Romy, Madhubanti Bagchi, Vivek Hariharan', 'B Praak, Akshay Kumar, Nupur Sanon, Ammy Virk', 'Yashita Sharma', 'Javed-Mohsin, Stebin Ben, Shreya Ghoshal', 'Shaan', 'Gajendra Verma, Flipsyde', 'Kasyap', 'Piyush Ambhore', 'Dev Arijit', 'B Praak, Sunny Singh', 'Raveena Paul', 'Divij Naik', 'Rahul Sathu, Ikka', 'Raj Barman, Raju Das', 'Adnan Sami', 'Shafqat Amanat Ali, Naveed Nashad', 'Amaal Mallik, Kaushal Kishore', 'Alka Yagnik, Daboo Malik', 'Harish Sagane, Soham Naik', 'Darshan Raval', 'Palak Muchhal, Zain Imam, Sana Khan', 'Simar Sethi', 'Amruta Fadnavis', 'Prateek Bajpai', 'Yasser Desai', 'Soham Naik', 'Raj Barman', 'Anurag Mohn', 'Jubin Nautiyal, Vishal Mishra, Raj Shekhar', 'Antara Mitra, Mohammed Irfan', 'Mohit Chauhan, Asees Kaur, Shubham-Ana', 'Prini Siddhant Madhav', 'Simar Sethi', 'King, Arjun Kanungo', 'Shriya Jain, Purabi Bhargava, Utkarsh Gupta', 'Harshavardhan Wavre, Leena Bose', 'Mohammed Irfan', 'Simar Sethi', 'Amaal Mallik, Kunaal Vermaa', 'Siddharth Kasyap, Mohammed Irfan, Kumaar', 'Rahul Jain', 'Stebin Ben', 'Nikhita Gandhi, Shankar Mahadevan ', 'Paras Chopra', 'Mohammed Irfan', 'Amit Mishra, Fukra Insaan', 'Rabbit Sack C', 'Arijit Singh, Somesh Saha', 'Rahul Jain', 'Shahid Mallya', 'Afsana Khan', 'Yasser Desai', 'Vishal Mishra', 'Jaani, Afsana Khan', 'Shaan', 'Jasleen Royal', 'Yasser Desai, Kunaal Vermaa', 'Stebin Ben, Siddharth Kasyap, Kumaar', 'Vishal Mishra', 'AKASA', 'Yasser Desai, Tanveer Evan, Rajat Nagpal', 'Pawan Singh, Salim-Sulaiman', 'Vishal Mishra, Rupali Jagga', 'Arijit Singh, Jasleen Royal', 'B Praak', 'Stebin Ben, Aman Pant', 'Pritam, Arijit Singh', 'Darshan Raval, Javed-Mohsin', 'A. R. Rahman', 'Pritam, Darshan Raval, Antara Mitra', 'Jonita Gandhi, Hriday Gattani, A. R. Rahman', 'Sachin-Jigar, Jubin Nautiyal, Asees Kaur', 'Palak Muchhal']
+    
+    print("Songs count: " + str(len(Songs)))
+    print("Artist count: " + str(len(Artists)))
+
+    for song, artist in zip(Songs, Artists):
+        print(artist.split(", ")[0]+" - " + song)
+        download_music(artist.split(', ')[0], song, output_path)
 
     # song_list =[('Gulabi Sharara', 'Inder Arya'), ('Cream Paudara Mashakbeen Uttrakhandi', 'Maya Upadhyay'), ('Dhai Hathe Dhameli Mashakbeen ( Feat. Manoj Arya, Priyanka Meher )', 'Manoj Arya, Priyanka Meher'), ('Hey Madhu Mashakbeen', 'Inder Arya'), ('Mathu Mathu Uttrakhandi', 'Inder Arya, Jyoti Arya'), ('Aawa Dida Bhulo...Thando Re Thando', 'Narendra Singh Negi'), ('Inder Arya', 'Mero Lehanga'), ('Nazar Na Lago Pahadi', 'Inder Arya'), ('Modern Kumaun Pahadi', 'Inder Arya'), ('Kamar Pida', '-'), ('Thando Re Thando', 'Meena Rana, Narendra Singh Negi, Anuradha Nirala'), ('Mera Dandi Kanthiyon Ka Muluk', 'Meena Rana, Narendra Singh Negi'), ('Sapna Syali Garhwali DJ Song', 'Saurav Maithani, Anisha Ranghar, Sanjay Bhandari, Raj Tiger'), ('Dyo Lagi', 'Gunjan Dangwal, Vivek Nautiyal'), ('Fyonladiya Twe Dekhik', 'Kishan Mahipal'), ('Bol Heera', 'Inder Arya'), ('Baand Bijora', 'Narendra Singh Negi'), ('Balma', 'Sanjay Kumola, Meena Rana'), ('Hafta Me 2.0 (Uttrakhandi)', 'Inder Arya, Meena Rana'), ('Jhumki', 'Darshan Farswan'), ('Pyari Nirmala', 'Saurav Maithani'), ('Mero Lehenga 2', 'Inder Arya, Jyoti Arya'), ('Saniyo Ma', 'Sanjay Kumola, Meena Rana, Ajay Solanki'), ('Mashakbeena Mashakbeen (Uttrakhandi)', 'Meena Rana'), ('Rajula (Feat. Inder Arya)', 'Inder Arya'), ('Nandre Tu', 'Rohit Chauhan'), ('Surju', 'Sanjay Kumola, Meena Rana'), ('Shiv Jatadhari Bhairav', 'Darshan Farswan'), ('Mathu Mathu Hitali Meri Bana Uttrakhandi', 'Jitendra Tomkyal'), ('Tera Nakhra', 'Rohit Chauhan'), ('Gajra', 'Sanjay Bhandari, Anisha Ranghar'), ('Heera Samdhini', 'Gajender Rana'), ('Ghuguti Ghuron Laigi', 'Meena Rana, Narendra Singh Negi'), ('Lehenga 4', 'Inder Arya, Mamta Arya'), ('Bedu Pako Baramasa', 'Meena Rana, Manglesh Dangwal, Janardan Prasad Nautiyal, Mukesh Kathait'), ('Lehenga 3 Kumauni album', 'Jyoti Arya, Inder Arya'), ('Santu Chori', 'Gajendra Rana'), ('Lehenga 3', 'Inder Arya, Jyoti Arya'), ('Meri Bhanuli', 'Inder Arya'), ('Bol Heera 2', 'Inder Arya, Jyoti Arya'), ('Main Chun Nauni Pauri Ki', 'Anisha Ranghar, Raj Tiger'), ('Bangaal Choori', 'Kishan Mahipal'), ('Chaita Ki Chaitwala (Jaagar)', 'Chandra Singh Rahi'), ('Insta Baand', 'Vijay Prakash'), ('Meri Jogni', 'Mohan Bisht, Anisha Ranghar'), ('Mohini', 'Rohit Chauhan'), ('Hit Madhuli 2', 'Inder Arya'), ('Thando Re Thando', 'Instrumental'), ('Rangeela Dupatta', 'Keshar Panwar, Anisha Ranghar'), ('Laadi Chandra', 'Anisha Ranghar, Sanjay Bhandari'), ('Garhwal Ki Daandi Kaathi', '-')]
-    output_path = './downloads/gharwali'
     # for i in song_list:
     #     download_music(i[1], i[0],output_path)
