0f17bdd79ff9b32195999671cfc6fa0cb2a79b8f
Shah-jigar <shah.jigar2002@gmail.com>
2024-03-31 12:05:36 +0530

    started working on inserting file code

--- mongoDB.py
+++ mongoDB.py
@@ -1,16 +1,47 @@
-from pprint import pprint
-import gridfs
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from bson import ObjectId, Binary
 from gridfs import GridFS
 from dotenv import load_dotenv, find_dotenv
-import os
 import pandas as pd
-from time import time
+import librosa
+from librosa import feature
+import numpy as np
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import joblib
+
+from pprint import pprint
 
 load_dotenv(find_dotenv())
 connections = {'local': None, 'remote': None}
+
+# For getting Feature Vector
+fn_list_i = [
+    librosa.onset.onset_strength,              # it is spectral_flux
+    # chromagram from a waveform or power spectrogram
+    feature.chroma_stft,
+    feature.chroma_cqt,
+    feature.chroma_cens,
+    feature.melspectrogram,
+    feature.mfcc,
+    feature.spectral_centroid,
+    feature.spectral_bandwidth,
+    feature.spectral_contrast,
+    feature.spectral_rolloff,
+    feature.tonnetz
+]
+
+fn_list_ii = [
+    feature.zero_crossing_rate
+]
+
+
+def get_feature_vector(y, sr):
+    feat_vect_i = [np.mean(funct(y=y, sr=sr)) for funct in fn_list_i]
+    feat_vect_ii = [np.mean(funct(y=y)) for funct in fn_list_ii]
+    feature_vector = feat_vect_i + feat_vect_ii
+    return feature_vector
 
 
 def get_connection(level, db_name):
@@ -144,6 +175,108 @@ def load_songs_csv(csv_path, types, db, collections,):
     pprint(lst)
 
 
+def create_dataset(collections):
+
+    pipeline = [
+        {
+            "$lookup": {
+                "from": 'song_features',
+                "localField": "features_id",
+                "foreignField": "_id",
+                "as": "matched_data"
+            }
+        },
+        {
+            "$unwind": "$matched_data"
+        },
+        {
+            "$project": {
+                "_id": 1,
+                "Song Name": 1,
+                "artist": 1,
+                "lyrics": 1,
+                "onset_strength": "$matched_data.onset_strength",
+                "chroma_stft": "$matched_data.chroma_stft",
+                "chroma_cqt": "$matched_data.chroma_cqt",
+                "chroma_cens": "$matched_data.chroma_cens",
+                "melspectrogram": "$matched_data.melspectrogram",
+                "mfcc": "$matched_data.mfcc",
+                "spectral_centroid": "$matched_data.spectral_centroid",
+                "spectral_bandwidth": "$matched_data.spectral_bandwidth",
+                "spectral_contrast": "$matched_data.spectral_contrast",
+                "spectral_rolloff": "$matched_data.spectral_rolloff",
+                "tonnetz": "$matched_data.tonnetz",
+                "zero_crossing_rate": "$matched_data.zero_crossing_rate",
+                "genre": 1,
+                "emotion": 1
+            }
+        }
+    ]
+    result = pd.DataFrame(db.songs.aggregate(pipeline))
+    print(result)
+    columns = result.columns.tolist()
+    columns.remove("genre")
+    columns.remove("emotion")
+    columns.extend(["genre", "emotion"])
+    result = result[columns]
+    result.to_csv('dataset')
+
+
+def load_model(type):
+    loaded_model = joblib.load(
+        f"C:/Projects/python/FYPBackend/models/best_Random Forest.joblib") or None
+    return loaded_model
+
+
+def insertSong(collections, path):
+    current_dir = os.getcwd()
+    path = path.replace('\\', '/')
+    dir = os.path.join(current_dir, path)
+    dir = dir.replace('\\', '/')
+
+    print(path)
+    print(dir)
+
+    full = False
+    if dir != path:
+        path = dir
+    y, sr = None, None
+    print("Extracting featrues from file")
+    # try:
+    #     full=True
+    #     y, sr = librosa.load(path, sr=None)
+    # except:
+    y, sr = librosa.load(dir, sr=None)
+    feature_vector = get_feature_vector(y, sr)
+    print(feature_vector)
+
+    labels_emotion = {0: "Happy", 1: "Sad", 2: "Love", 3: "Energetic"}
+    labels_genre = {0: "Hindi Rap", 1: "Ghazal", 2: "Gharwali",
+                    3: "Bhajan", 4: "Hindi Romantic", 5: "Sufi"}
+
+    model = load_model('genre') or None
+    genre = labels_genre[tuple(model.predict([feature_vector]))[0]]
+
+    model = load_model('genre') or None
+    emotion = labels_emotion[tuple(model.predict([feature_vector]))[0]]
+
+    # Left
+    x = path.split('/')[-1]
+    artist, song = x.split(" - ", 1)
+    song = song.split(".")[0]
+    data = {"Song Name": song, "artist": artist}
+    print(data)
+    data = data+feature
+    data['genre'] = genre or ""
+    data['emotion'] = emotion or ""
+
+    create_document(collection=collections[0], data=data)
+
+    # till here
+
+    return genre, emotion
+
+
 if __name__ == "__main__":
     os.system('clear')
 
@@ -152,23 +285,11 @@ if __name__ == "__main__":
     collection0 = db['songs']
     collection1 = db['song_features']
 
-    load_songs_csv(csv_path='Songs/genres/all_features_947.csv',
-                   types='genres', db=db, collections=[collection0, collection1])
-    load_songs_csv(csv_path='Songs/emotions/all_features_emotion_559.csv',
-                   types='emotions', db=db, collections=[collection0, collection1])
+    # load_songs_csv(csv_path='Songs/genres/all_features_947.csv',
+    #                types='genres', db=db, collections=[collection0, collection1])
+    # load_songs_csv(csv_path='Songs/emotions/all_features_emotion_559.csv',
+    #    types='emotions', db=db, collections=[collection0, collection1])
 
-    # Update entry
-    # update_query = {'_id': ObjectId('65efeafe5a6afb9cf404edbc')}
-    # new_data = {"city": "Mumbai"}
-    # update_document(collection= collection0,query=update_query, new_data=new_data)
-
-    # Read documents
-    # read_documents()
-
-    # Delete a document
-    # delete_query = {"name": "John Doe"}
-    # delete_document(delete_query)
-
-    # Read documents
-    # myquery = { "city": {"$gt": "N"} }
-    # read_documents(myquery)
+    insertSong(collections=[collection0, collection1],
+               path='download/Tanishk Bagchi - Teri Baaton Mein Aisa Uljha Jiya.opus')
+    # create_dataset(collections=[collection0, collection1])
